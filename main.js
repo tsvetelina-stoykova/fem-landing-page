11ac185ba2f8e0d6b63e2c992e2cc8d446f358c1
tsevetelina-stoykova <cveti.stoikova@gmail.com>
2022-08-17 18:38:49 +0300

    + mobile navigation

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const nav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+	nav.hasAttribute("data-visible") 
+	? navToggle.setAttribute("aria-expanded", false) 
+	: navToggle.setAttribute("aria-expanded", true)
+	nav.toggleAttribute("data-visible");
+})
 
